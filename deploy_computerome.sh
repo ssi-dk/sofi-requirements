340eaf81149952ea84eee69f54b771d2987ec384
kikris <krki@ssi.dk>
2024-02-20 08:58:23 +0100

    Changed filenames to ease automatic deployment

---
+++ deploy_computerome.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -euo pipefail
+
+checkout () {
+  component=$1
+  branch=${2:-main}
+  pushd ../components
+  if [ ! -e bifrost_$component ]
+  then
+    git clone git@github.com:ssi-dk/bifrost_$component.git
+  fi
+  cd bifrost_$component
+  git pull
+  popd
+}
+
+copy_custom_content () {
+  component=$1
+  component_dir=../components/bifrost_$component
+  cp ${component}.yml $component_dir/environment.yml
+  cp install.sh $component_dir/
+  if [ -e ${component}_ci.sh ]
+  then
+     cp ${component}_ci.sh $component_dir/custom_install.sh
+  fi
+}
+
+get_component_name() {
+  local __resvar=$1
+  local COMPONENT_NAME=$(grep "display_name:.*." $CONFIG_YAML_PATH | tr " " "\n" | grep -v "display_name:")
+  eval $__resvar="'$COMPONENT_NAME'"
+}
+
+get_component_version() {
+  local __resvar=$1
+  local COMPONENT_VERSION=$(grep -o "code:.*." $CONFIG_YAML_PATH | tr " " "\n" | grep -v "code:")
+  eval $__resvar="'$COMPONENT_VERSION'"
+}
+
+get_environment_name() {
+  local __resvar=$1
+  local SCRIPT_DIR=${2:-.}
+  local CONFIG_YAML_PATH=$(find $SCRIPT_DIR -name "config.yaml")
+  get_component_name name
+  get_component_version version
+  local _env_name=("bifrost_"$name"_"$version)
+  eval $__resvar="'$_env_name'"
+}
+
+install_component () {
+  component=$1
+  component_dir=../components/bifrost_$component
+  pushd $component_dir
+  set +e
+  bash ./install.sh -i COMP
+  if [ $? -ne 0 ]
+  then
+    echo "Failed to install $component. Attempting to continue"
+  fi
+  set -e
+  get_environment_name env_name
+  conda activate $env_name
+  local CONFIG_YAML_PATH=$(find . -name "config.yaml")
+  get_component_name name
+  component_name=("bifrost_"$name)
+  python -m $component_name -h
+  conda deactivate
+  popd
+}  
+
+arg1=${1:-}
+if [[ -z "$arg1" ]]
+then
+  echo "$#"
+  components=`cat components.txt`
+else
+  components="$@"
+fi
+
+branch="test"
+
+## Enable conda
+module load tools $CONDA_VERSION
+eval "$(conda shell.bash hook)"
+
+
+for component in $components
+do
+  checkout $component $branch;
+  copy_custom_content $component;
+  install_component $component;
+done
